de8e4652395868f8d8fc6a31205c7989fe121cc5
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2022-05-15 09:22:46 -0300

    added formatting options to the timed function

--- pyprofile/pyprofile.py
+++ pyprofile/pyprofile.py
@@ -3,11 +3,17 @@ import logging as logger
 
 
 #%% decorator to log the time taken on various activitites.
-def timed(func):
+def timed(func, desc=None, append_name=False, append_template = "{} - {}"):
+    
+    fn_name = desc if desc else func.__qualname__
+    
+    if append_name:
+        fn_name = append_template.format(fn_name, func.__qualname__)
+
     def wrapper(*args, **kwargs):
         ret = None
         try:
-            logger.info(f"Timing function {func.__qualname__}")
+            logger.info(f"Timing function {fn_name}")
             initTime = time.time()
             ret = func(*args, **kwargs)
             return ret
@@ -17,6 +23,6 @@ def timed(func):
         finally:
             endTime = time.time()
             totalTimeInMilis = round((endTime - initTime) * 1000)
-            logger.info(f"Function call ({func.__qualname__}) took {totalTimeInMilis} ms")
+            logger.info(f"Function call ({fn_name}) took {totalTimeInMilis} ms")
     
     return wrapper
